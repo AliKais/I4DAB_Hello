98288641f16b4f8c327f4123e6e8269767ab7fcd
florent417 <flole@live.dk>
2019-04-01 11:11:44 +0200

    Added classes to folder model and data

---
+++ Whist_DAB/Whist_DAB/Data/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whist_DAB.Data
+{
+    class Class2
+    {
+    }
+}
